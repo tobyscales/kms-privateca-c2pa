205bdc1b96013e86d2deb6b253bde3713c85a335
Toby Scales <17342265+tobyscales@users.noreply.github.com>
2025-12-03 07:13:20 -0700

    Update provision_cert.py

--- c2pa-project/scripts/provision_cert.py
+++ c2pa-project/scripts/provision_cert.py
@@ -1,3 +1,7 @@
+## Copyright 2025 Google. 
+## This software is provided as-is, without warranty or representation for any use or purpose. 
+## Your use of it is subject to your agreement with Google.  
+
 import os
 import sys
 from google.cloud import kms
